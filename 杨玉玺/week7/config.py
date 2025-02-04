3b6c75a3393f5f024f8a9c96b5bf2f397ab8ffc7
luoxi163 <yangyuxi202206@163.com>
2025-02-04 14:32:44 +0800

    Create config.py

---
+++ 杨玉玺/week7/config.py
@@ -0,0 +1,22 @@
+"""
+配置参数信息
+"""
+
+Config = {
+    "model_path": "output",
+    "train_data_path": "train_data.txt",
+    "valid_data_path": "valid_data.txt",
+    "vocab_path":"chars.txt",
+    "model_type":"bert",
+    "max_length": 30,
+    "hidden_size": 256,
+    "kernel_size": 3,
+    "num_layers": 2,
+    "epoch": 10,
+    "batch_size": 16,
+    "pooling_style":"avg",
+    "optimizer": "adam",
+    "learning_rate": 1e-5,
+    "pretrain_model_path":r"F:\NLP\NLP-07\bert-base-chinese",
+    "seed": 987
+}
